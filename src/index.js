da3c4990d0b3774cbc594ec52676872c61899586
zkan <kan@prontomarketing.com>
2017-03-15 10:11:15 +0700

    Implement 2 functions: add and area

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('Hello, Node!')
+
+// Single return statement -- use (..)
+const add = (a, b) => (a + b)
+
+// Multiple statements -- use {..}
+const area = (r) => {
+  const PI = 3.14
+  return PI * r * r
+}
+
+console.log('3 + 5 = ' + add(3, 5))
+console.log('area = ' + area(3))
