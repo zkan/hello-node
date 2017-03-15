575efa089d84b0ffb0bdc014bcdd0a03a636ad53
zkan <kan@prontomarketing.com>
2017-03-15 10:14:42 +0700

    Try destructure

--- src/index.js
+++ src/index.js
@@ -11,3 +11,15 @@ const area = (r) => {
 
 console.log('3 + 5 = ' + add(3, 5))
 console.log('area = ' + area(3))
+
+// Destructure
+const props = {
+  width: 100,
+  height: 200,
+  type: 'text',
+  radius: 10,
+}
+
+const {width, height} = props
+console.log(width)
+console.log(height)
