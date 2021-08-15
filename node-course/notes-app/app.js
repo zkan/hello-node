69e1d11b6aa94552965bd2828f7e65187c9be32f
zkan <zkan.cs@gmail.com>
2021-08-15 21:40:27 +0700

    Import own file

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,4 +1,9 @@
 const fs = require('fs')
+require('./utils')
 
 fs.writeFileSync('notes.txt', 'This file was created by Node.js!\n')
 fs.appendFileSync('notes.txt', 'My name is Kan.')
+
+const name = 'Kan'
+
+console.log(name)
