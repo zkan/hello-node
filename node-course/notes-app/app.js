fa56bf696dcaa0eb5bc385f92f29e5c10234579f
zkan <zkan.cs@gmail.com>
2021-08-15 21:32:53 +0700

    Append text to file

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,3 +1,4 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+fs.writeFileSync('notes.txt', 'This file was created by Node.js!\n')
+fs.appendFileSync('notes.txt', 'My name is Kan.')
