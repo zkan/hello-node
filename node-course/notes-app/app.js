18fcace2fe4687ec0e8824b0c11a19e96f28938d
zkan <zkan.cs@gmail.com>
2021-08-16 18:51:27 +0700

    Set options when add a new note

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -47,8 +47,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: () => {
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log('Title:', argv.title)
+        console.log('Body:', argv.body)
     }
 })
 
@@ -79,4 +92,5 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+// console.log(yargs.argv)
+yargs.parse()
