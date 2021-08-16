f6a31098d61b25eb0d91c82e00751669ea01748f
zkan <zkan.cs@gmail.com>
2021-08-16 08:01:44 +0700

    Try adding yargs command :bear:

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const chalk = require('chalk')
 const validator = require('validator')
+const yargs = require('yargs')
 const { someName, add } = require('./utils')
 const getNotes = require('./notes')
 
@@ -24,3 +25,58 @@ console.log(validator.isEmail('zkan@hey'))
 console.log(chalk.green('Success!'))
 console.log(chalk.red.inverse('Failed!'))
 console.log(chalk.blue('Hello') + ' World' + chalk.red('!'))
+
+// Get args with process
+console.log(process.argv)
+console.log(process.argv[2])
+
+const command = process.argv[2]
+
+if (command === 'add') {
+    console.log('Adding note')
+} else if (command === 'remove') {
+    console.log('Removing note')
+}
+
+// Get args with yargs
+
+// Customize yargs version then run `node app.js --version`
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Removing the note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: () => {
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log('Reading a note')
+    }
+})
+
+console.log(yargs.argv)
