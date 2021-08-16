aed22cec30c3f40dc669b79d10ba043da9465aee
zkan <zkan.cs@gmail.com>
2021-08-16 07:24:25 +0700

    Try to validate email

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const validator = require('validator')
 const { someName, add } = require('./utils')
 const getNotes = require('./notes')
 
@@ -15,3 +16,6 @@ console.log(sum)
 
 const notes = getNotes()
 console.log(notes)
+
+console.log(validator.isEmail('zkan@hey.com'))
+console.log(validator.isEmail('zkan@hey'))
