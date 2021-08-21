a589e48e4756b5c53fac74a275625eea9cd46d42
zkan <zkan.cs@gmail.com>
2021-08-21 11:23:22 +0700

    Let request parse data for us :bear:

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -14,9 +14,11 @@ console.log('Stopping')
 
 const url = 'http://api.weatherstack.com/current\?access_key\=f3cbc3c5605f1d274565b7fa5c8b8d83\&query\=37.8267,-122.4233'
 
-request({ url }, (error, response) => {
-  console.log(response)
-  const data = JSON.parse(response.body)
-  console.log(data)
-  console.log(data.current)
+request({ url, json: true }, (error, response) => {
+  //console.log(response)
+  //const data = JSON.parse(response.body)
+  //console.log(data)
+  //console.log(data.current)
+
+  console.log(response.body.current)
 })
