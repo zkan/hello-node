bf2f3a71d805d48e03292f5e2b9c86b21d677c69
zkan <zkan.cs@gmail.com>
2021-08-22 09:31:09 +0700

    Destructure response :bear:

--- node-course/weather-app/utils/forecast.js
+++ node-course/weather-app/utils/forecast.js
@@ -3,17 +3,17 @@ const request = require('postman-request')
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current\?access_key\=f3cbc3c5605f1d274565b7fa5c8b8d83\&query\=' + latitude + ',' + longitude + '&unit=f'
   console.log(url)
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
        callback('Unable to connect to weatherstack service!', undefined)
-    } else if (response.body.error) {
-      const errorInfo = response.body.error.info
+    } else if (body.error) {
+      const errorInfo = body.error.info
       callback(errorInfo, undefined)
     } else {
-      console.log(response.body)
-      const weather_descriptions = response.body.current.weather_descriptions
-      const temperature = response.body.current.temperature
-      const feelslike = response.body.current.feelslike
+      console.log(body)
+      const weather_descriptions = body.current.weather_descriptions
+      const temperature = body.current.temperature
+      const feelslike = body.current.feelslike
 
       const forecastResult = weather_descriptions[0] + '. It is currently ' + temperature + ' degress out. It feels like ' + feelslike + ' degress out.'
 
--- node-course/weather-app/utils/geocode.js
+++ node-course/weather-app/utils/geocode.js
@@ -2,16 +2,16 @@ const request = require('postman-request')
 
 const geocode = (address, callback) => {
   const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiemthbiIsImEiOiJja3NsYWh0dzAwZ2o2Mm9zb21sZjhkdHhrIn0.j-e0byTFMGhTduZpwB4LHg'
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
        callback('Unable to connect to mapbox service!', undefined)
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
        callback('Unable to find location. Try another search.', undefined)
     } else {
-      console.log(response.body)
-      const longitude = response.body.features[0].center[0]
-      const latitude = response.body.features[0].center[1]
-      const location = response.body.features[0].place_name
+      console.log(body)
+      const longitude = body.features[0].center[0]
+      const latitude = body.features[0].center[1]
+      const location = body.features[0].place_name
       console.log(longitude, latitude)
 
       callback(undefined, {
