1312d103bafc0a7c1b13af17102170df1621df35
zkan <zkan.cs@gmail.com>
2021-08-25 07:53:24 +0700

    Send back response in HTML and JSON :bear:

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -3,19 +3,31 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-  res.send('Hello Express!')
+  res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-  res.send('Help page')
+  res.send([
+    {
+      name: 'Kan',
+      age: 25,
+    },
+    {
+      name: 'Salee',
+      age: 20
+    }
+  ])
 })
 
 app.get('/about', (req, res) => {
-  res.send('About')
+  res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
-  res.send('Your weather')
+  res.send({
+    forecast: 'It is snowing',
+    location: 'Philadelphia',
+  })
 })
 
 app.listen(3000, () => {
