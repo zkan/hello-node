76e326dc522aa44a2e06252e25ffdd42c2c66e10
zkan <zkan.cs@gmail.com>
2021-08-25 08:10:08 +0700

    Create about and help pages + remove the routes for now :bear:

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -11,23 +11,6 @@ const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-  res.send([
-    {
-      name: 'Kan',
-      age: 25,
-    },
-    {
-      name: 'Salee',
-      age: 20
-    }
-  ])
-})
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>')
-})
-
 app.get('/weather', (req, res) => {
   res.send({
     forecast: 'It is snowing',
