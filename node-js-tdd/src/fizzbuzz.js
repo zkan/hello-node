dfad3bde4ad68d3d9f8b4df9d3597c85e63af489
zkan <zkan.cs@gmail.com>
2024-12-17 10:15:28 +0700

    Implement fizzbuzz func

--- node-js-tdd/src/fizzbuzz.js
+++ node-js-tdd/src/fizzbuzz.js
@@ -1,3 +1,10 @@
 export function fizzbuzz(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return 'FizzBuzz'
+    } else if (number % 3 === 0) {
+        return 'Fizz'
+    } else if (number % 5 === 0) {
+        return 'Buzz'
+    }
     return String(number)
 }
--- node-js-tdd/test/fizzbuzz.test.js
+++ node-js-tdd/test/fizzbuzz.test.js
@@ -15,3 +15,15 @@ import { fizzbuzz } from '../src/fizzbuzz'
 test('given 1 should return 1', () => {
     expect(fizzbuzz(1)).toEqual('1')
 })
+
+test('given 3 should return Fizz', () => {
+    expect(fizzbuzz(3)).toEqual('Fizz')
+})
+
+test('given 5 should return Buzz', () => {
+    expect(fizzbuzz(5)).toEqual('Buzz')
+})
+
+test('given 15 should return FizzBuzz', () => {
+    expect(fizzbuzz(15)).toEqual('FizzBuzz')
+})
