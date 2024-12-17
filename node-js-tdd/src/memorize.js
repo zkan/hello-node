b26149712d491ab52dafeefa2171eb31cbfe0a6a
zkan <zkan.cs@gmail.com>
2024-12-17 11:52:22 +0700

    Implement match and unmatch

--- node-js-tdd/src/memorize.js
+++ node-js-tdd/src/memorize.js
@@ -1,3 +1,8 @@
+// Card
+// - emoji: string
+// - isFaceUp: boolean
+// - isMatched: boolean
+
 export class Memorize {
     cards = []
     constructor(emojis) {
@@ -10,8 +15,22 @@ export class Memorize {
         }
 
         for (let emoji of Array.from(emojis)) {
-            this.cards.push(emoji)
-            this.cards.push(emoji)
+            this.cards.push({ emoji, isFaceUp: false })
+            this.cards.push({ emoji, isFaceUp: false })
         }
+    }
+
+    choose(index) {
+        const facedUpCardIndex = this.cards.findIndex((card) => card.isFaceUp)
+        if (facedUpCardIndex != -1) {
+            if (this.cards[facedUpCardIndex].emoji == this.cards[index].emoji) {
+                this.cards[facedUpCardIndex].isMatched = true
+                this.cards[index].isMatched = true
+            } else {
+                this.cards[facedUpCardIndex].isFaceUp = false
+                this.cards[index].isFaceUp = false
+            }
+        }
+        this.cards[index].isFaceUp = true
     }
 }
--- node-js-tdd/test/memorize.test.js
+++ node-js-tdd/test/memorize.test.js
@@ -8,25 +8,67 @@ import { Memorize } from '../src/memorize'
 // '😆🥶' -> [Card(😆), Card(😆), Card(🥶), Card(🥶)]
 
 // play game
-// 
+// choose first card should face up
+// choose faced up card should remain the same
+// choose card after matched, the matched card should disappear
+// choose card after unmatched, the unmatched card should face down
 
-describe('int', () => {
-    it('init with empty string should throw error "empty string"', () => {
-        expect(() => { new Memorize('') }).toThrowError('empty string')
+describe('game', () => {
+    describe('int', () => {
+        it('init with empty string should throw error "empty string"', () => {
+            expect(() => { new Memorize('') }).toThrowError('empty string')
+        })
+    
+        it('inti with "😆" should init with two cards', () => {
+            const game = new Memorize('😆')
+            expect(game.cards.length).toEqual(2)
+        })
+    
+        it('inti with "😆😆" should throw error "duplicate emoji"', () => {
+            const game = 
+            expect(() => { new Memorize('😆😆') }).toThrowError('duplicate emojis')
+        })
+    
+        it('inti with "😆🥶" should init with four cards', () => {
+            const game = new Memorize('😆🥶')
+            expect(game.cards.length).toEqual(4)
+        })
+
+        it('inti with "😆🥶" should init with four cards with all face down', () => {
+            const game = new Memorize('😆🥶')
+            expect(game.cards.length).toEqual(4)
+            game.cards.forEach(card => {
+                expect(card.isFaceUp).toEqual(false)
+            })
+        })
     })
 
-    it('inti with "😆" should init with two cards', () => {
-        const game = new Memorize('😆')
-        expect(game.cards.length).toEqual(2)
-    })
+    describe('game play', () => {
+        it('choose first card should face up', () => {
+            const game = new Memorize('😆🥶')
+            expect(game.cards[0].isFaceUp).toEqual(false)
+            game.choose(0)
+            expect(game.cards[0].isFaceUp).toEqual(true)
+        })
 
-    it('inti with "😆😆" should throw error "duplicate emoji"', () => {
-        const game = 
-        expect(() => { new Memorize('😆😆') }).toThrowError('duplicate emojis')
-    })
+        it('choose card after matched, the matched card should disappear', () => {
+            const game = new Memorize('😆🥶')
+            game.choose(0)
+            game.choose(1)
+            game.choose(2)
+            expect(game.cards[0].isMatched).toEqual(true)
+            expect(game.cards[1].isMatched).toEqual(true)
+        })
 
-    it('inti with "😆🥶" should init with four cards', () => {
-        const game = new Memorize('😆🥶')
-        expect(game.cards.length).toEqual(4)
+        it('choose card after unmatched, the unmatched card should face down', () => {
+            const game = new Memorize('😆🥶')
+            game.choose(0)
+            game.choose(2)
+            game.choose(1)
+            expect(game.cards[0].isFaceUp).toEqual(false)
+            expect(game.cards[2].isFaceUp).toEqual(false)
+        })
     })
 })
+
+
