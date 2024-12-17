30751898c72e99120517a15327ca9a7bef439636
zkan <zkan.cs@gmail.com>
2024-12-17 11:21:57 +0700

    Throw error when get duplicate emojis

--- node-js-tdd/src/memorize.js
+++ node-js-tdd/src/memorize.js
@@ -1,7 +1,17 @@
 export class Memorize {
+    cards = []
     constructor(emojis) {
         if (!emojis) {
             throw new Error('empty string')
+        }
+
+        if (Array.from(emojis).length != Array.from(new Set(emojis)).length) {
+            throw new Error('duplicate emojis')
+        }
+
+        for (let emoji of Array.from(emojis)) {
+            this.cards.push(emoji)
+            this.cards.push(emoji)
         }
     }
 }
--- node-js-tdd/test/memorize.test.js
+++ node-js-tdd/test/memorize.test.js
@@ -4,14 +4,24 @@ import { Memorize } from '../src/memorize'
 // init
 // '' -> throw error 'empty string
 // '😆' -> [Card(😆), Card(😆)]
-// '😆😆' -> throw error 'duplicate'
+// '😆😆' -> throw error 'duplicate emoji'
 // '😆🥶' -> [Card(😆), Card(😆), Card(🥶), Card(🥶)]
 
 // play game
 // 
 
 describe('int', () => {
-    it('init with empty string should throw error', () => {
+    it('init with empty string should throw error "empty string"', () => {
         expect(() => { new Memorize('') }).toThrowError('empty string')
+    })
+
+    it('inti with "😆" should init with two cards', () => {
+        const game = new Memorize('😆')
+        expect(game.cards.length).toEqual(2)
+    })
+
+    it('inti with "😆😆" should throw error "duplicate emoji"', () => {
+        const game = 
+        expect(() => { new Memorize('😆😆') }).toThrowError('duplicate emoji')
     })
 })
