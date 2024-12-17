183c06847f2e9c6d7cc1fb62acc99d8ad4525e69
zkan <zkan.cs@gmail.com>
2024-12-17 14:31:37 +0700

    Set up seed

---
+++ node-js-tdd/src/seed.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    // Seed the database
+    await prisma.theme.create({
+        data: {
+            name: 'Animals',
+            emojis: '😆'
+        },
+    })
+
+    await prisma.theme.create({
+        data: {
+          name: 'Fruits',
+          emojis: '🍏🍎🍐🍊🍋'
+        },
+    })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
