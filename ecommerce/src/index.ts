df155eb04a59e550d1e980de8782a89dbbca8c74
zkan <zkan.cs@gmail.com>
2026-01-01 11:57:03 +0700

    Separate login to its concern

--- ecommerce/src/index.ts
+++ ecommerce/src/index.ts
@@ -1,11 +1,10 @@
 import express, { Express, Request, Response } from "express"
 import { PORT } from "./secrets"
+import rootRouter from "./routes"
 
 const app: Express = express()
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working")
-})
+app.use("/api", rootRouter)
 
 app.listen(PORT, () => {
   console.log("App's working")
