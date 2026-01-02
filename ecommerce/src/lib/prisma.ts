6952c216ca767be020f1c052bd1c2714325f3b6a
zkan <zkan.cs@gmail.com>
2026-01-02 09:40:19 +0700

    Remove logging

--- ecommerce/src/lib/prisma.ts
+++ ecommerce/src/lib/prisma.ts
@@ -3,7 +3,6 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
-console.log(connectionString)
 
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({
