c52b09f9bf1a693e63c2833e329757312c08bc53
zkan <zkan.cs@gmail.com>
2026-01-02 09:40:34 +0700

    Add script to test database connection

---
+++ ecommerce/src/script.ts
@@ -0,0 +1,16 @@
+import { prisma } from './lib/prisma'
+
+async function main() {
+  const allUsers = await prisma.user.findMany()
+  console.log('All users:', JSON.stringify(allUsers, null, 2))
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
