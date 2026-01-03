92a5a3fa3e4b244b5b4752b64d749da39b32a21f
zkan <kan@odds.team>
2026-01-03 21:17:01 +0700

    Configure jest

---
+++ ecommerce/jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+};
